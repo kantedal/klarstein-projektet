3f7bc03d5736072d5c46e94d5aa476d0a04c03fc
Jacob Pålsson <jacpa975@student.liu.se>
2018-05-27 21:55:57 +0200

    Nice fucking commit

--- src/Pay/Pay.tsx
+++ src/Pay/Pay.tsx
@@ -1,5 +1,7 @@
+import { OrderList } from '../models/OrderList'
 import * as React from 'react'
 import './Pay.css'
+import Button from 'material-ui/Button'
 
 namespace Pay {
   export interface Props {
@@ -8,13 +10,27 @@ namespace Pay {
 
   }
 }
-
+const ordersList: OrderList = [{
+  name: 'Norrlands king',
+  id: "asd",
+  quantity: 23,
+  price: 22,
+  img: ''
+}]
 class Pay extends React.Component<Pay.Props, Pay.State> {
   render () {
-    const {children} = this.props
+    const {} = this.props
     return (
       <div className={'pay-container'}>
-        {children}
+        asd
+        <div className={'buttons-container'}>
+          <Button variant="outlined" color="primary" className={'pay-button'}>
+            Bakåt
+          </Button>
+          <Button variant="outlined" color="default" className={'pay-button'}>
+            OK
+          </Button>
+        </div>
       </div>
     )
   }
